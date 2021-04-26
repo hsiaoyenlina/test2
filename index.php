49e2425bf574d7ebc896e38b410210b157c6ac97
LinaChen <hsiaoyenlina@gmail.com>
2021-04-26 15:48:00 +0800

    加入test2文件

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>  
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>第一個JavaScript程式</title>
+	<script language="javascript">
+      
+        alert("Hello JavaScript!");
+      
+    </script>
+  </head>	
+  <body> 
+	<?php
+		echo "現在是PHP的顯示";
+	  	echo "<br>這是第二列文字"
+	?>	
+  </body>
+</html>
